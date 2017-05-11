bf448d7f45d53999e7302c6d2abc990c4ab0a4b5
leaalex <macpro@mac-pro-mac.at.urfu.ru>
2017-05-11 15:21:03 +0500

    обработка роута получение id

--- src/app/program-disciplines-constructor/program-disciplines-constructor.component.ts
+++ src/app/program-disciplines-constructor/program-disciplines-constructor.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import { Subscription } from 'rxjs/Subscription';
+
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-program-disciplines-constructor',
@@ -6,8 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./program-disciplines-constructor.component.scss']
 })
 export class ProgramDisciplinesConstructorComponent implements OnInit {
+  
+  private subscription: Subscription;
 
-  constructor() { }
+  public errorMessage: string;
+
+    constructor ( private router: Router,
+                private activateRoute: ActivatedRoute,
+                private titleService: Title,
+                private globalService: GlobalService
+              ) { 
+                    this.subscription = activateRoute.params.subscribe(
+                      params => {
+                        console.log(params['id']);
+                      },
+                      error => this.errorMessage = "Неверный адрес!"
+                    );
+                    router.events.subscribe(
+                      (val) => {
+                        this.errorMessage = "";
+                      }
+                    );
+                  }
 
   ngOnInit() {
   }
