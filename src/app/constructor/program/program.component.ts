921c63a07a1a5c51d8cb58c02b61c27e60584074
leaalex <lea_alex@mail.ru>
2017-06-06 15:03:22 +0500

    подключение targets

--- src/app/constructor/program/program.component.ts
+++ src/app/constructor/program/program.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
 import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/switchMap';
 
 import { ConstructorService } from '../constructor.service';
 
@@ -12,6 +13,8 @@ import { ConstructorService } from '../constructor.service';
   styleUrls: ['./program.component.scss']
 })
 export class ProgramComponent implements OnInit {
+  private program;
+  private targets;
 
   constructor( private router: Router,
                private activateRoute: ActivatedRoute,
@@ -19,6 +22,18 @@ export class ProgramComponent implements OnInit {
                private service: ConstructorService ) { }
 
   ngOnInit() {
-  }
+  this.activateRoute.params
+    .switchMap((params: Params) => this.service.getElementsBySlug('programs', params['id']))
+    .subscribe((program) => {
+      this.program = program;
+      this.titleService.setTitle(this.program.title);
+      this.service.getElementsBySlug('get_targets_by_program', this.program.id)
+                  .subscribe(
+                    targets => {
+                      this.targets = targets;
+                    }
+                  )
+    });
+}
 
 }
