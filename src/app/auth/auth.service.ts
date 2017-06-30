bbc95ea630804450e065abd8cf3d3aac00f2636f
vesloguzov <vesloguzov@gmail.com>
2017-06-30 15:00:46 +0500

    дщпщге

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -24,10 +24,12 @@ export class AuthService {
                 localStorage.setItem('currentUser', JSON.stringify(user));
             }
             console.log('Вы авторизованы!', user);
+            location.reload();
             });
     }
 
     logout() {
+        location.reload();
         localStorage.removeItem('currentUser');
         this.router.navigate(['login']);
     }
