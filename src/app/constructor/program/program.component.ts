3118f8d93a40586cc57c8f581480d634ce1fbf82
leaalex <lea_alex@mail.ru>
2017-07-13 15:58:40 +0500

    фильтрация & online

--- src/app/constructor/program/program.component.ts
+++ src/app/constructor/program/program.component.ts
@@ -35,6 +35,7 @@ export class ProgramComponent implements OnInit {
   public eduLength = [1,2,3,4,5,6,7,8];
   public term = '4 года';
   public presence: string = 'z';
+  public technology_type = 'd';
 
   public funcLength(val, term) {
     this.eduLength = val;
@@ -43,6 +44,10 @@ export class ProgramComponent implements OnInit {
 
   public setPresence(val){
     this.presence = val;
+  }
+
+  public setTechnologyType(val){
+    this.technology_type = val;
   }
 
   constructor( private router: Router,
