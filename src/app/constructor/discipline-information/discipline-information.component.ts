8a4d2a1d2117a50503b9e2d0147cd4f509fcedeb
MasterGowen <mastergowen@gmail.com>
2017-10-18 15:57:02 +0500

    diagram height, competence length < 100%

--- src/app/constructor/discipline-information/discipline-information.component.ts
+++ src/app/constructor/discipline-information/discipline-information.component.ts
@@ -28,7 +28,7 @@ export class DisciplineInformationComponent implements OnInit {
     console.log(this.discipline.title, this.discipline.default_semester, this.program.variants[this.discipline.id])
   }
 
-  toogle_view(){
+  toogle_view() {
     this.show_variants = !this.show_variants;
   }
 
@@ -36,28 +36,25 @@ export class DisciplineInformationComponent implements OnInit {
     if (this.trajectory.getVariantSelected(this.discipline.id)) {
         this.pieChartData['labels'] = [];
         this.pieChartData['datasets'] = [];
-        
-          const diagram = this.trajectory.getVariantSelected(this.discipline.id).diagram.diagram;
-          if (diagram.length > 0){
-            for (let i = 1; i < 21; i++ ) {
-              this.pieChartData['labels'].push('Неделя ' + i);
-            }
-            for (let q = 0; q < diagram.length; q++) {
-              this.pieChartData['datasets'].push({
-                'label': diagram[q]['wt'],
-                'stack': 'stack 0',
-                'data': Object.keys(diagram[q]).map(key => diagram[q][key]).filter(labor => typeof labor === 'number')
-              });
-            }
+        const diagram = this.trajectory.getVariantSelected(this.discipline.id).diagram.diagram;
+        console.log("diagram", this.trajectory.getVariantSelected(this.discipline.id));
+        if (diagram.length > 0) {
+          for (let i = 1; i < 21; i++ ) {
+            this.pieChartData['labels'].push('Неделя ' + i);
           }
+          for (let q = 0; q < diagram.length; q++) {
+            this.pieChartData['datasets'].push({
+              'label': diagram[q]['wt'],
+              'stack': 'stack 0',
+              'data': Object.keys(diagram[q]).map(key => diagram[q][key]).filter(labor => typeof labor === 'number')
+            });
+          }
+        }
           return this.pieChartData;
         } else {
           return false;
         }
-        
   }
- 
-
   ngOnInit() {
         this.trajectory = this.data.trajectory;
         this.program = this.data.program;
