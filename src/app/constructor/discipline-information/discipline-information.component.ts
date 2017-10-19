3b6bbdd2869a6cd162cf314fe750091ae45478a9
MasterGowen <mastergowen@gmail.com>
2017-10-19 15:11:28 +0500

    diagrams colors, order

--- src/app/constructor/discipline-information/discipline-information.component.ts
+++ src/app/constructor/discipline-information/discipline-information.component.ts
@@ -6,6 +6,8 @@ import { DataService } from '../data.service';
 import { Trajectory } from '../../models/trajectory';
 import { Program } from '../../models/program';
 
+import {ChartsModule, Color} from 'ng2-charts';
+
 @Component({
   selector: '[app-discipline-information]',
   templateUrl: './discipline-information.component.html',
@@ -18,6 +20,29 @@ export class DisciplineInformationComponent implements OnInit {
   public pieChartType = 'bar';
   public pieChartLabels: string[] = [];
   public pieChartData: any = {};
+  public colorsOverride: Array<Color> = [
+  {
+    backgroundColor: 'red',
+  },
+  {
+    backgroundColor: '#4be2ea'
+  },
+  {
+    backgroundColor: '#2da910'
+  },
+  {
+    backgroundColor: '#e00c76'
+  },
+  {
+    backgroundColor: '#e0db0c'
+  },
+  {
+    backgroundColor: '#999999'
+  },
+  {
+    backgroundColor: '#69e'
+  }
+];
   public show_variants = false;
 
   @Input() discipline;
@@ -35,19 +60,20 @@ export class DisciplineInformationComponent implements OnInit {
         this.pieChartData['labels'] = [];
         this.pieChartData['datasets'] = [];
         const diagram = this.trajectory.getVariantSelected(this.discipline.id).diagram.diagram;
-        console.log("diagram", this.trajectory.getVariantSelected(this.discipline.id));
         if (diagram.length > 0) {
           for (let i = 1; i < 21; i++ ) {
             this.pieChartData['labels'].push('Неделя ' + i);
           }
-          for (let q = 0; q < diagram.length; q++) {
+          let q = diagram.length;
+          while (q--) {
             this.pieChartData['datasets'].push({
-              'label': diagram[q]['wt'],
-              'stack': 'stack 0',
-              'data': Object.keys(diagram[q]).map(key => diagram[q][key]).filter(labor => typeof labor === 'number')
+              label: diagram[q]['wt'],
+              stack: 'stack 0',
+              data: Object.keys(diagram[q]).map(key => diagram[q][key]).filter(labor => typeof labor === 'number')
             });
           }
         }
+          console.log(this.pieChartData)
           return this.pieChartData;
         } else {
           return false;
